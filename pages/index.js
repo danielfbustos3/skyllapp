c09898eef94822f738b043637f9a54b40d52fef1
danielfbustos3 <danielfbustos3@gmail.com>
2022-05-07 14:40:22 -0500

    Provisional Skyll

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import styled from "@emotion/styled";
+import Logo from "../components/logo";
+import { keyframes } from "@emotion/react";
+
+const goUp = keyframes`
+0%, 70% {
+  transform: translateY(60%);
+}
+100% {
+  transform: translateY(0);
+}
+`;
+
+const fadeIn = keyframes`
+0%, 80% {
+  opacity: 0;
+}
+100% {
+  opacity: 1;
+}
+`;
+
+const LandingContainer = styled.main`
+  height: 100vh;
+  width: 100%;
+  /* background: red; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  svg {
+    animation: ${goUp} 3s;
+    height: 150px;
+  }
+  h1 {
+    text-align: center;
+    animation: ${fadeIn} 3.5s;
+  }
+`;
 
 export default function Home() {
   return (
@@ -11,59 +50,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <LandingContainer>
+        <Logo />
+        <h1>
+          We are building an incredible website! <br />
+          Just for you...
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      </LandingContainer>
     </div>
-  )
+  );
 }
